7b5818c5686a6cbeabc11948310d03941c439aed
Kimsua99 <79245006+Kimsua99@users.noreply.github.com>
2022-04-03 02:43:28 +0900

    [Bronze II] Title: 블랙잭, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.cc
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+
+int main()
+{
+	int n, m;
+	int arr[100];
+
+	int result = 0;
+	int sum = 0;
+
+	scanf("%d %d", &n, &m);
+	
+	for (int i = 0; i < n; i++)
+	{
+		scanf("%d", &arr[i]);
+	}
+
+	for (int i = 0; i < n - 2; i++)
+	{
+		for (int j = i + 1; j < n - 1; j++)
+		{
+			for (int k = j + 1; k < n; k++)
+			{
+				sum = arr[i] + arr[j] + arr[k];
+
+				if (sum <= m && sum > result)
+				{
+					result = sum;
+				}
+			}
+		}
+	}
+
+	printf("%d", result);
+
+	return 0;
+
+}
