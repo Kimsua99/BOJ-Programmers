1f328ac090b91809612068f3d05df88c6a494faa
수아 <79245006+Kimsua99@users.noreply.github.com>
2023-06-18 01:10:00 +0900

    [Silver II] Title: 유기농 배추, Time: 0 ms, Memory: 2036 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.cc
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.cc
@@ -1,74 +1,96 @@
-#include <bits/stdc++.h>
+#include<vector>
+#include<queue>
+#include<bits/stdc++.h>
 using namespace std;
 
-#define X first
-#define Y second
+int board[51][51] = { 0 };
+bool vis[51][51] = { 0 };
 
-int board[51][51];
-bool vis[51][51];
-int dx[4] = { 1,0,-1,0 };
-int dy[4] = { 0,1,0,-1 };
+int dx[4] = { 1, 0, -1, 0 };
+int dy[4] = { 0, 1, 0, -1 };
 
-int m, n, k;
-
-queue <pair<int, int >> q;
-
-int main(void) 
+int t, n, m, k, x, y, cnt;
+vector<int> v;
+void bfs(int n, int m)
 {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    int t;
-    cin >> t;//테스트 케이스
-    while (t--) 
-    {
-        cin >> m >> n >> k;// 가로길이, 세로길이, 심어져있는 개수
+	queue<pair<int, int>> q;
+	cnt = 0;
 
-        int x, y;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			if (board[i][j] == 1 && vis[i][j] == false)
+			{
+				q.push({ i, j });
+				vis[i][j] = true;
+				cnt++;
 
-        for (int i = 0; i < k; i++) 
-        {
-            cin >> x >> y;
-            board[y][x] = 1;//심어져 있는 부분을 1로 설정
-        }
+				while (!q.empty())
+				{
+					int curX = q.front().first;
+					int curY = q.front().second;
 
-        int res = 0; //지렁이 개수
+					q.pop();
+					
+					for (int k = 0; k < 4; k++)
+					{
+						int nx = curX + dx[k];
+						int ny = curY + dy[k];
 
-        for (int i = 0; i < n; i++) 
-        {
-            for (int j = 0; j < m; j++) 
-            {
-                if (board[i][j] == 1 && !vis[i][j]) //이미 방문 한 경우
-                {
-                    vis[i][j] = true;
-                    q.push({ i,j });//방문 표시 남기고 푸시
-                    
-                    while (!q.empty())
-                    {
-                        auto cur = q.front(); q.pop();//현재는 큐의 맨 앞이고 팝해서 빼냄
+						if (nx < 0 || nx >= n || ny < 0 || ny >= m)
+							continue;
+						if (board[nx][ny] == 0 || vis[nx][ny] == true)
+							continue;
+						q.push({ nx, ny });
+						vis[nx][ny] = true;
+						//board[curX][curY] = board[nx][ny] + 1;
+					}
 
-                        for (int dir = 0; dir < 4; dir++)//상하좌우 돌면서 확인
-                        {
-                            int nx = cur.X + dx[dir];
-                            int ny = cur.Y + dy[dir];
-                            if (nx < 0 || nx >= n || ny < 0 || ny >= m)//범위 밖이면 다음 반복
-                                continue;
-                            if (vis[nx][ny] || board[nx][ny] != 1)//방문했으면 다음 반복
-                                continue;
+				}
+			}
+		}
+	}
 
-                            vis[nx][ny] = true;//방문 표시 남기고 푸시
-                            q.push({ nx,ny });
-                        }
-                    }
-                    res++;//지렁이 개수 하나 더 증가
-                }
-            }
-        }
-        cout << res << "\n";
-        for (int i = 0; i < n; i++) 
-        {
-            fill(board[i], board[i] + m, 0);
-            fill(vis[i], vis[i] + m, false);//다음 테스트 케이스 돌아야 하므로 기존 판 모두 0으로 바꾸고 방문 여부도 false로 변경
-        }
-    }
-    return 0;
+	v.push_back(cnt);
+	
+}
+void reset() 
+{
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			board[i][j] = 0;
+		}
+	}
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			vis[i][j] = 0;
+		}
+	}
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> t;
+
+	for (int i = 0; i < t; i++)
+	{
+		cin >> n >> m >> k;
+
+		for (int j = 0; j < k; j++)
+		{
+			cin >> x >> y;
+			board[x][y] = 1;
+		}
+
+		bfs(n, m);
+		reset();
+	}
+
+	for (int i = 0; i < v.size(); i++)
+		cout << v[i] << '\n';
 }
