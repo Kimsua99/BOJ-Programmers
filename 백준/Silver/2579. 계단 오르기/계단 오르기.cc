3292d4a64bd2310ba4cc14ec7e81377a08fb1c1d
수아 <79245006+Kimsua99@users.noreply.github.com>
2023-06-21 00:56:05 +0900

    [Silver III] Title: 계단 오르기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.cc
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.cc
@@ -8,21 +8,14 @@ int arr[301] = { 0, };
 int main() 
 {
     cin >> N;
-
     for (int i = 1; i <= N; i++)
         cin >> arr[i];
 
     DP[1] = arr[1];
-
     DP[2] = arr[1] + arr[2];
-
     DP[3] = max(arr[1], arr[2]) + arr[3];
+    for (int i = 4; i <= N; i++)
+        DP[i] = max(DP[i - 3] + arr[i - 1], DP[i - 2]) + arr[i];
 
-    for (int j = 4; j <= N; j++) 
-    {
-        DP[j] = max(DP[j - 2] + arr[j], DP[j - 3] + arr[j - 1] + arr[j]);
-    }
-
-    cout << DP[N];
-    return 0;
+    cout << DP[N] << '\n';
 }
