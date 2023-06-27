bd7b77f738b854082a1ab2eefd1252c090f3ee4d
수아 <79245006+Kimsua99@users.noreply.github.com>
2023-06-28 07:24:10 +0900

    [Silver III] Title: N과 M (3), Time: 540 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m, cnt;
+int arr[10];
+vector<int> v;
+
+void DFS(int cnt) 
+{
+    if (cnt == m) 
+    {
+        for (int i = 0; i < v.size(); i++)
+            cout << v[i] << " ";
+        cout << '\n';
+        return;
+    }
+
+    for (int i = 1; i <= n; i++) 
+    {
+        v.push_back(i);
+        DFS(cnt + 1);
+        v.pop_back();
+    }
+}
+
+int main(void) 
+{
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++)
+        arr[i] = i + 1;
+
+    DFS(0);
+}
