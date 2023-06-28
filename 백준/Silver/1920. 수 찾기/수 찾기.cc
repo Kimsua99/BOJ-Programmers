ec7dde1e32797d41235364433b89c479881c0ba5
수아 <79245006+Kimsua99@users.noreply.github.com>
2023-06-28 16:53:41 +0900

    [Silver IV] Title: 수 찾기, Time: 52 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -1,43 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int a[1000005];
-int n;
+int n, m;
+int arr[100005];
 
-int binarysearch(int target)
+int search(int target)
 {
-    int st = 0;
-    int en = n - 1;
-    while (st <= en)//시작점이 끝점보다 앞에 있거나 같을 경우 반복
-    {
-        int mid = (st + en) / 2;
-        if (a[mid] < target)//중간보다 목표가 뒤에 있으면 중간 앞부분은 안봐도 됨
-            st = mid + 1; //시작 지점을 중간 바로 다음으로 잡아서 탐색
-        else if (a[mid] > target)//중간보다 목표가 앞에 있으면 중간 뒷부분은 안봐도 됨
-            en = mid - 1; //끝 지점을 중간 바로 이전으로 잡아서 탐색
-        else
-            return 1;//중간 지점이 목표인 경우이므로 1 반환
-    }
-    return 0;//st > en 인 경우이므로 while문 탈출, 0 반환
+	int st = 0;
+	int en = n - 1;
+
+	while (st <= en)
+	{
+		int mid = (st + en) / 2;
+		if (arr[mid] < target)
+			st = mid + 1;
+		else if (arr[mid] > target)
+			en = mid - 1;
+		else
+			return 1;
+	}
+	return 0;
 }
-int main(void)
-{
-    ios::sync_with_stdio(0);
-    cin.tie(0);
 
-    cin >> n;
-    for (int i = 0; i < n; i++)
-    {
-        cin >> a[i];
-    }
-    sort(a, a + n);
-    int m;
-    cin >> m;
-    while (m--)
-    {
-        int t;
-        cin >> t;
-        cout << binarysearch(t) << '\n';
-    }
-     
+
+int main(void) 
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cin >> n;
+	for (int i = 0; i < n; i++)
+		cin >> arr[i];
+	sort(arr, arr + n);
+
+	cin >> m;
+	while (m--)
+	{
+		int mnum;
+		cin >> mnum;
+		cout << search(mnum) << '\n';
+	}
 }
