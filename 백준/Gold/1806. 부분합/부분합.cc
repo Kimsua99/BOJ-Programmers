6568522d4044c9ab487d42c59fbf9f7b7fcf126d
수아 <79245006+Kimsua99@users.noreply.github.com>
2023-06-30 07:12:09 +0900

    [Gold IV] Title: 부분합, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.cc
+++ 백준/Gold/1806. 부분합/부분합.cc
@@ -9,28 +9,31 @@ int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
+
     cin >> n >> s;
 
-    for (int i = 0; i < n; i++) 
+    for (int i = 0; i < n; i++)
         cin >> a[i];
-    tot = a[0];
+
     int en = 0;
 
-    for (int st = 0; st < n; st++) 
+    tot = a[0];
+
+    for (int st = 0; st < n; st++)
     {
-        while (en < n && tot < s) 
+        while (en < n && tot < s)
         {
             en++;
-            if (en != n) tot += a[en];
+            if (en != n)
+                tot += a[en];
         }
-
-        if (en == n) break; // en이 범위를 벗어날시 종료
-
-        mn = min(mn, en - st + 1);
-
-        tot -= a[st];
+        if (en == n)
+            break;
+        mn = min(mn, en - st + 1);//개수 구하기
+        tot -= a[st];//맨 앞지점 뺐는데도 합이 s 이상인지 확인하기 위해
     }
-    if (mn == 0x7fffffff) 
+    if (mn == 0x7fffffff)
         mn = 0;
     cout << mn;
+
 }
