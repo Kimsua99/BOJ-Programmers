f7951901ece66a7e83bb1cfe6d7255fc1acbcfb1
수아 <79245006+Kimsua99@users.noreply.github.com>
2023-07-02 00:51:03 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 92 ms, Memory: 13084 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.cc
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.cc
@@ -1,24 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-unordered_set<string> s; //중복 허용이 안되는 집합같은 stl
-
-int main(void)
+set<string> s;
+int n; 
+string name;
+string st;
+int main(void) 
 {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
-  int n;
-  cin >> n;
-  while(n--)
-  {
-    string name, log;
-    cin >> name >> log;
-    if(log == "enter") //출근의 경우
-        s.insert(name);//해당 이름 추가
-    else s.erase(name);//퇴근의 경우 해당 이름 제거
-  }
-  vector<string> ans(s.begin(), s.end());//결과를 담을 벡터 배열
-  sort(ans.begin(), ans.end(), greater<string>());//greater -> 역순 정렬
-  for(auto x : ans) cout << x << '\n';
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> name >> st;
+        if (st == "enter")
+            s.insert(name);
+        if (st == "leave")
+        {
+            s.erase(name);
+        }
+    }
+
+    vector<string> ans(s.begin(), s.end());
+
+    sort(ans.begin(), ans.end(), greater<string>());
+
+    for (auto x : ans) 
+        cout << x << '\n';
 }
