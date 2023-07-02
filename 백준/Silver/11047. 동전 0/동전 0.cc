bc50ca29622ac2ea946924e8e79e36b3791c55cb
수아 <79245006+Kimsua99@users.noreply.github.com>
2023-07-03 01:20:24 +0900

    [Silver IV] Title: 동전 0, Time: 0 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.cc
+++ 백준/Silver/11047. 동전 0/동전 0.cc
@@ -1,25 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, k;//n개의 동전 종류, 가치의 합 k
-int a[15];
+int n, k;
+
+int a[1000001];
+int ans;
 
 int main(void) 
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    int ans = 0;
     cin >> n >> k;
 
-    for (int i = 0; i < n; i++) //n종류를 for문 돌면서 배열에 입력받음. 이때 가치는 오름차순으로 입력해야함.
+    for (int i = 0; i < n; i++)
         cin >> a[i];
 
-    for (int i = n - 1; i >= 0; i--) //가장 가치가 높은 수부터 가치 낮은 동전까지 차례로 반복문 실행
+    for (int i = n - 1; i >= 0; i--)
     {
-        ans += k / a[i];//결과 값은 가치합을 각 동전의 가치로 나눈 값을 더한 결과.
-        k %= a[i];//k는 각 동전의 가치로 가치합을 나눴을 때 남은 결과가 됨. 
+        if (a[i] <= k)
+        {
+            ans += k / a[i];
+            k = k % a[i];
+        }
+        else
+            continue;
     }
-
     cout << ans;
+
 }
