508e732490aa0418d3fa4bce8127859e2794760c
수아 <79245006+Kimsua99@users.noreply.github.com>
2023-07-04 00:06:04 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool flag = false;
+string s = "";
+int ans;
+string st;
+int main(void)
+{
+	cin >> s;
+
+	for (int i = 0; i <= s.size(); i++)
+	{
+		if (s[i] == '-' || s[i] == '+' || i == s.size())
+		{
+			if (flag == true)//앞서 -가 있었을 경우
+			{
+				ans -= stoi(st);
+				st = "";
+			}
+			else
+			{
+				ans += stoi(st);
+				st = "";
+			}
+		}
+		else
+		{
+			st += s[i];
+		} 
+		if (s[i] == '-')
+			flag = true;
+	}
+	cout << ans;
+}
