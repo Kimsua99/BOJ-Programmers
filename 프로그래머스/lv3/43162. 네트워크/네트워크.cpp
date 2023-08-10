203c66efd808f3e561809222334fc188da74e3c0
수아 <79245006+Kimsua99@users.noreply.github.com>
2023-08-10 23:18:20 +0900

    [level 3] Title: 네트워크, Time: 0.41 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/네트워크.cpp
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.cpp
@@ -1,33 +1,36 @@
 #include <string>
 #include <vector>
-#include <iostream>
 
 using namespace std;
 
-int check[200];
+int vis[201];
+int answer = 0;
 
-void dfs(int cur_com, int n, vector<vector<int>> computers)
+void dfs(int st, int n, vector<vector<int>> computers)
 {
-    check[cur_com] = 1;
-
-    for (int i = 0; i < n; i++)
+    vis[st] = 1;
+    
+    for(int i = 0; i < n; i++)
     {
-        if (check[i] == 0 && computers[cur_com][i] == 1)//방문한 적이 없고 다른 컴퓨터와 연결되어 있으면
-            dfs(i, n, computers);//dfs로 탐색 시작
+        if(vis[i] == 0 && computers[st][i] == 1)
+        {
+            dfs(i, n, computers);
+        }
     }
 }
 
 int solution(int n, vector<vector<int>> computers) 
 {
-    int answer = 0;
-
-    for (int i = 0; i < n; i++)
+    
+    for(int i = 0; i < n; i++)
     {
-        if (check[i] == 0)//방문하지 않은 지점이면
+        if(vis[i] == 0)
         {
             dfs(i, n, computers);
-            answer++;//처음 시작 지점이므로 ++하고 연결이 끊어져 다시 돌아와 시작할때도 ++한다.
+            answer++;
         }
     }
+    
+    
     return answer;
 }
