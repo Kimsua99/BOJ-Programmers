7495de43b38e68e3963a70d4da1d33047d5d1e42
수아 <79245006+Kimsua99@users.noreply.github.com>
2023-08-11 22:35:22 +0900

    [level 3] Title: 숫자 게임, Time: 10.51 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/12987. 숫자 게임/숫자 게임.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int solution(vector<int> A, vector<int> B) 
+{
+    int answer = 0;
+    
+    sort(A.begin(), A.end());
+    sort(B.begin(), B.end());
+    
+    int idx = 0;
+    
+    for(int i = 0; i < A.size(); i++)
+    {
+        if(A[idx] < B[i])
+        {
+            idx++;
+            answer++;
+        }
+    }
+    
+    return answer;
+}
