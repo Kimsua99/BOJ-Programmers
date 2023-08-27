b088701a745e7f21a0be0a1fbf975c287dc812d2
수아 <79245006+Kimsua99@users.noreply.github.com>
2023-08-28 07:40:23 +0900

    [Bronze IV] Title: !밀비 급일, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11365. ！밀비 급일/！밀비 급일.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+
+#include <string>
+
+using namespace std;
+
+ 
+
+int main(void)
+
+{
+
+        ios_base::sync_with_stdio(0);
+
+        cin.tie(0);
+
+       
+
+        while (1)
+
+        {
+
+                 string s;
+
+                 getline(cin, s);
+
+ 
+
+                 if (s == "END")
+
+                         break;
+
+ 
+
+                 for (int i = s.length() - 1; i >= 0; i--)
+
+                         cout << s[i];
+
+                 cout << "\n";
+
+        }
+
+        return 0;
+
+}
