47151970934ea0cbd691ba304e5f3df60dd4fcc6
수아 <79245006+Kimsua99@users.noreply.github.com>
2023-09-13 18:57:56 +0900

    [Bronze III] Title: 최소, 최대, Time: 464 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.cc
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+int num;
+
+int main()
+{
+	int minValue = 1000001;
+	int maxValue = -1000001;
+
+	int N;
+	cin >> N;
+
+	int inputValue;
+	for (int i = 0; i < N; i++) 
+	{
+		cin >> inputValue;
+
+		if (inputValue < minValue) 
+		{
+			minValue = inputValue;
+		}
+		if (inputValue > maxValue) 
+		{
+			maxValue = inputValue;
+		}
+	}
+
+	cout << minValue << " " << maxValue;
+	return 0;
+}
