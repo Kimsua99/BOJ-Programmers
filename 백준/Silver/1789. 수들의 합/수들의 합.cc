4dbe0c793976ed8feaffe2d63ab35048fd29e336
수아 <79245006+Kimsua99@users.noreply.github.com>
2023-09-14 01:02:30 +0900

    [Silver V] Title: 수들의 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1789. 수들의 합/수들의 합.cc
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+ll S;
+bool chk(ll n){ return n*(n+1)/2 > S; }
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cin >> S;
+    ll l = 1, r = 93000; // sqrt(4294967295 * 2)
+    while(l <= r){
+        ll mid = (l + r) / 2;
+        if(chk(mid))r = mid - 1;
+        else l = mid + 1;
+    }
+    cout << r;
+}
