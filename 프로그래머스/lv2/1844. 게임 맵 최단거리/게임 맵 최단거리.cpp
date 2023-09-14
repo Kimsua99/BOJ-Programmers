f4fc6245a12a3be4d40170593e923c2f5ab2ac3e
수아 <79245006+Kimsua99@users.noreply.github.com>
2023-09-14 18:32:27 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 0.36 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
@@ -1,57 +1,56 @@
 #include<vector>
-#include<queue>
+#include <bits/stdc++.h>
 using namespace std;
-
-int n, m;//열, 행
-
-int board[101][101] = { 0 }; // 진행 거리 체크
-bool vis[101][101] = { 0 }; // 방문 여부 체크
-
-int dx[4] = { 1,0,-1,0 };
-int dy[4] = { 0,1,0,-1 };
-
-queue<pair<int, int>> q;
 
 int solution(vector<vector<int> > maps)
 {
-    int answer = 0;
-
-    n = maps[0].size();//열
-    m = maps.size();//행
-
-    vis[0][0] = true;//처음 지점 방문 체크해줌.
-    board[0][0] = 1;
-    q.push(make_pair(0,0));//처음 지점 큐에 넣음.
-
-    //bfs 시작
-    while (!q.empty())//큐가 빌 때 까지 큐의 front를 pop으로 빼내고 상하좌우 살피면서 큐에 push 해주는 작업 반복
+    int ans = 0;
+    
+    int board[101][101];
+    int vis[101][101];
+    
+    int dx[4] = {1, 0, -1, 0};
+    int dy[4] = {0, 1, 0, -1};
+    
+    queue<pair<int, int>>q;
+    
+    for(int i = 0; i < maps.size(); i++)
     {
-        int curX = q.front().first;//맨 앞 좌표
-        int curY = q.front().second;
-
-        q.pop();
-
-        for (int i = 0; i < 4; i++)//상하좌우 확인
+        for(int j = 0; j < maps[0].size(); j++)
         {
-            int nx = curX + dx[i];
-            int ny = curY + dy[i];
+            q.push({0,0});
+            vis[0][0] = 1;
+            board[0][0] = 1;
 
-            if (nx < 0 || nx >= m || ny < 0 || ny >= n)//범위 벗어난 경우 다음 반복
-                continue;
+            while(!q.empty())
+            {
+                int curX = q.front().first;
+                int curY = q.front().second;
 
-            if (vis[nx][ny] || maps[nx][ny] == 0)//이미 방문했거나 벽인 경우 다음 반복
-                continue;
+                q.pop();
+                
+                for(int k = 0; k < 4; k++)
+                {
+                    int nx = curX + dx[k];
+                    int ny = curY + dy[k];
 
-            vis[nx][ny] = true;//방문 표시 남김
-            q.push(make_pair(nx,ny));//큐에 넣음
-            board[nx][ny] = board[curX][curY] + 1;
+                    if(nx < 0 || ny < 0 || nx >= maps.size() || ny >= maps[0].size())
+                        continue;
+                    if(maps[nx][ny] == 0 || vis[nx][ny] == 1)
+                        continue;
+                    
+                    q.push({nx,ny});
+                    vis[nx][ny] = 1;
+                    board[nx][ny] = board[curX][curY] + 1;
+                }
+            }
         }
     }
 
-    if (!board[m - 1][n - 1])
-        answer = -1;
+     if (!board[maps.size() - 1][maps[0].size() - 1])//상대팀 진영에 도착할 수 없는 경우
+        ans = -1;
     else
-        answer = board[m - 1][n - 1];
+        ans = board[maps.size() - 1][maps[0].size() - 1]; // 상대팀 진영 위치에서의 카운트 값 출력
 
-    return answer;
+    return ans;
 }
