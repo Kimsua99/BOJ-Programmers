3f93e6c9e9c55c463dfce00969025ee84e4a6108
수아 <79245006+Kimsua99@users.noreply.github.com>
2023-09-14 19:34:08 +0900

    [Silver II] Title: 사탕 게임, Time: 40 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.cc
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using namespace std;
+int N, maxCandy = 0;
+char candy[51][51];
+
+void check();
+
+int main(void) 
+{
+
+    cin >> N;
+
+    for (int i = 0; i < N; i++) 
+    {
+        for (int j = 0; j < N; j++) 
+        {
+            cin >> candy[i][j];
+        }
+    }
+
+    for (int i = 0; i < N; i++) 
+    {
+        for (int j = 0; j < N - 1; j++) 
+        {
+            swap(candy[i][j], candy[i][j + 1]);
+            check();
+            swap(candy[i][j], candy[i][j + 1]);
+        }
+    }
+
+    for (int j = 0; j < N; j++)
+    {
+        for (int i = 0; i < N - 1; i++) 
+        {
+            swap(candy[i][j], candy[i + 1][j]);
+            check();
+            swap(candy[i][j], candy[i + 1][j]);
+        }
+    }
+
+    cout << maxCandy << '\n';
+}
+
+void check() 
+{
+    for (int i = 0; i < N; i++) 
+    {
+        int count = 1;
+        for (int j = 0; j < N; j++) 
+        {
+            if (candy[i][j] == candy[i][j + 1]) 
+            {
+                count++;
+            }
+            else
+            {
+                if (maxCandy < count) maxCandy = count;
+                count = 1;
+            }
+        }
+    }
+
+    for (int j = 0; j < N; j++) 
+    {
+        int count = 1;
+        for (int i = 0; i < N; i++) 
+        {
+            if (candy[i][j] == candy[i + 1][j]) 
+            {
+                count++;
+            }
+            else 
+            {
+                if (maxCandy < count) maxCandy = count;
+                count = 1;
+            }
+        }
+    }
+}
