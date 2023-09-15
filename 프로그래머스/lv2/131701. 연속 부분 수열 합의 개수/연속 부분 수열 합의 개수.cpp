cd3da5154582baefe1d5eb3411472f8f6a170bea
수아 <79245006+Kimsua99@users.noreply.github.com>
2023-09-15 16:56:39 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 417.22 ms, Memory: 22.8 MB -BaekjoonHub

--- 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.cpp
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.cpp
@@ -4,35 +4,20 @@
 
 using namespace std;
 
-int solution(vector<int> elements)
-{
-    int answer = 0;
-    vector<int> arr(elements);
-    set<int> s;//중복 방지를 위해
+int solution(vector<int> elements) {
+    set<int> S;
 
-    for (int i = 0; i < elements.size() - 1; i++)//가장 마지막 원소 제외하고 기존 배열에 붙여서 원형으로 만듦
-    {
-        arr.push_back(elements[i]);
-    }
-    for (int i = 0; i < elements.size(); i++)
-    {
-        vector<int> v;
+    int n = elements.size();
 
-        for (int j = 0; j < elements.size(); j++)
+    for (int i = 0 ; i < n ; ++i) 
+    {
+        int sum = 0;
+        for (int j = i ; j < i + n ; ++j) 
         {
-            int num = 0;
-
-            for (int k = j; k < i + j + 1; k++)//부분 수열 합 벡터에 넣어줌.
-            {
-                num += arr[k];
-            }
-
-            v.push_back(num);
+            sum += elements[j % n];
+            S.insert(sum);
         }
-
-        for (int j = 0; j < v.size(); j++)//중복 제거를 위해 set에 v 벡터 값들을 넣어줌.
-            s.insert(v[j]);
     }
-    answer = s.size();//중복 제거된 값 개수 리턴
-    return answer;
+
+    return S.size();
 }
