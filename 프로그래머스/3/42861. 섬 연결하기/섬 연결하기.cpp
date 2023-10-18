608e533b1275481c8a9b1310932defd1590a60ac
수아 <79245006+Kimsua99@users.noreply.github.com>
2023-10-18 13:48:11 +0900

    [level 3] Title: 섬 연결하기, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.cpp
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.cpp
@@ -1,37 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int parent[101];
+int parent[101];//부모 집합
 
-int find(int x) 
+int find(int x)
 {
-    if(parent[x] == x) 
+    if(parent[x] == x)
         return x;
     else
         return parent[x] = find(parent[x]);
 }
 
-bool cmp(vector<int> a, vector<int> b) 
+bool cmp(vector<int> a, vector<int> b)
 {
     return a[2] < b[2];
 }
 
 int solution(int n, vector<vector<int>> costs) 
 {
-    int ans = 0;
+    int answer = 0;    
     
     for(int i = 0; i < n; i++)
         parent[i] = i;
     
     sort(costs.begin(), costs.end(), cmp);
     
-	 for(int i = 0; i < costs.size(); i++) 
-     {
-        if(find(costs[i][0]) != find(costs[i][1])) 
-        {  
-            ans += costs[i][2];
-            parent[find(costs[i][1])] = find(costs[i][0]);
+    for(int i = 0; i < costs.size(); i++)
+    {
+        int st = costs[i][0];
+        int en = costs[i][1];
+        int cost = costs[i][2];
+        
+        if(find(st) != find(en))
+        {
+            answer += cost;
+            parent[find(en)] = find(st);
         }
     }
-	return ans;
+    return answer;
 }
