dab5e0074bf687ee43dccaa9b2842e68acc04b8a
수아 <79245006+Kimsua99@users.noreply.github.com>
2024-02-08 23:58:55 +0900

    [Silver II] Title: 스택 수열, Time: 56 ms, Memory: 2416 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.cc
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+stack<int> st;
+vector<char> v;
+int main() 
+{
+	int n = 0;
+    int cnt = 1;
+	
+	cin >> n;
+	for (int i = 0; i < n; i++) 
+    {
+		int x;
+		cin >> x;
+		while (cnt <= x) 
+        {
+			v.push_back('+');
+			st.push(cnt);
+			cnt++;
+		}
+
+		if(x == st.top()) 
+        {
+			st.pop();
+			v.push_back('-');
+		}
+		else
+        {
+			cout << "NO";
+			return 0;
+		}
+	}
+    
+	for (int i = 0; i < v.size(); i++)
+    {
+		cout << v[i] << "\n";
+	}
+}
